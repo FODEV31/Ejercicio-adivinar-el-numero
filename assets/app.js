ca48c4d061ebbfbf1487878d10756ebe885bad98
FODEV31 <fiov1995@hotmail.com>
2026-06-08 22:26:28 -0400

    Arreglo de interfaz

--- assets/app.js
+++ assets/app.js
@@ -13,28 +13,34 @@ function adivinarNumero(secreto){
         if(verificarNumero(result)){
             lista.push(result);
             i++
+            
             if(result == secreto){
-                console.log("Numero correcto felicidaddes!!!");
-                break;
+                alert("Numero correcto felicidaddes!!!");
+                document.getElementById('historial').innerHTML = `Intentos: ${lista.join(', ')} (acierto)`;
+                return;
             }else{
-                console.log("No es el numero correcto intente nuevamente (intentos: "+ (i)+ " de 3");
+                alert("No es el numero correcto intente nuevamente (intentos: "+ (i)+ " de 3");
+                document.getElementById('historial').innerHTML = `Intentos: ${lista.join(', ')}`;
             }
         }
+    }
+    if(i == 3){
+        alert("Sin aciertos. El número era: " + secreto);
     }
 }
 
 function verificarNumero(numero){
     if(!Number.isInteger(numero)){
-        console.log("Numero ingresado no valido intente de nuevo");
+        alert("Numero ingresado no valido intente de nuevo");
         return false;
     }
     if (result<1 || result >10){
-        console.log("Numero ingresado fuera de los limites intente de nuevo");
+        alert("Numero ingresado fuera de los limites intente de nuevo");
         return false;
     }
 
     if(yaUsado(numero, lista)){
-        console.log("numero ya utilizado porfavor intente con otro numero");
+        alert("numero ya utilizado porfavor intente con otro numero");
         return false;
     }
     return true;
